53fac1f2e92c1373800d219f6706a3ed765019a0
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-05 16:30:22 -0300

    Criando arquivo intial.py
    Criando arquivo que exibe menu de opções ao usuário

---
+++ inicial.py
@@ -0,0 +1,19 @@
+import os
+
+opcao=True
+while opcao==True:
+    print("\n\n")
+    print("********************************")
+    print("****** EXERCÍCIOS REVISÃO ******")
+    print("********************************")
+    print("******** RAUL BARCELOS *********")
+    print()
+
+    print("********************************")
+    for i in range(1, 7):
+        menu = "******   {} → QUESTÃO 0{}   ******".format(i, i)
+        print(menu)
+    print("****   0 → FECHAR SISTEMA  *****")
+    print("********************************")
+    print()
+    opcao=False
