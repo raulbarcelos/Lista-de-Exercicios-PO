12752058dc2ee87a98cc505fa2d74f06de5a9e21
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-05 16:52:33 -0300

    Criando arquivo questao3.py
    Inicialmente criamos as funcionalidades para ler os arquivos e imprimir o conteúdo das matrizes.

---
+++ questoes/questao3.py
@@ -0,0 +1,46 @@
+arq = open("matriz.txt", 'r')
+arq2 = open("matriz2.txt", 'r')
+
+print("********************************")
+print("********** QUESTÃO 03 **********")
+print("********************************")
+print("******** RAUL BARCELOS *********")
+print()
+
+matriz_txt = []
+matriz2_txt = []
+
+matriz = []
+matriz2 = []
+
+
+# função para importar valores do arquivo de texto
+print("Importando arquivos txt e salvando em matriz...\n")
+def valores(matriz, matriz_txt, arquivo):
+    matriz_txt = arquivo.readlines()  # retorna o arquivo em forma de linha em listas, cada linha vira um elemento desse vetor
+    for i in range(len(matriz_txt)):
+        matriz.append(matriz_txt[i].split())
+    return matriz
+
+
+# função para imprimir matriz
+def imprime_matriz(matriz):
+    for i in range(len(matriz)):
+        print(matriz[i])
+    print()
+
+
+# função para imprimir os asteríscos
+def imprime_informacao(titulo):
+    print("*************** {} ****************".format(titulo))
+
+
+mat_a = valores(matriz, matriz_txt, arq)
+mat_b = valores(matriz2, matriz2_txt, arq2)
+imprime_informacao("MATRIZ A")
+imprime_matriz(mat_a)
+imprime_informacao("MATRIZ B")
+imprime_matriz(mat_b)
+
+arq.close()
+arq2.close()
