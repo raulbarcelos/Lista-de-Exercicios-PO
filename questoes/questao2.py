4ce908c824e9a81c0fe4fb120a58ce997983cea0
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-05 16:37:04 -0300

    Criando arquivo questão2.py
    Inicialmente criamos apenas a função que lê 10 números do usuário.

---
+++ questoes/questao2.py
@@ -0,0 +1,34 @@
+import random
+
+numeros = list()
+define_tamanho = 10
+
+print("********************************")
+print("********** QUESTÃO 02 **********")
+print("********************************")
+print("******** RAUL BARCELOS *********")
+print()
+
+
+
+def ler_dez_numero(tamanho, lista):
+    contador = 0
+    for i in range(tamanho):
+        i = contador
+        verd = True
+        while verd:
+            print("Digite o {}º número INTEIRO:".format(i+1), end=" ")
+            num = input("")
+            if not num.lstrip("-").isdigit() and num != "\n":
+                print("\nDigite apenas números!\n")
+            else:
+                verd = False
+        numeros.append(int(num))
+        contador+=1
+
+
+
+    imprimir(tamanho, lista, "digitados")
+
+
+
