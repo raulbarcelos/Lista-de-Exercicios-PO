951841b686e4d69e269394f30c3ca6b8697716a6
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-05 16:43:57 -0300

    Adicionando função pagina_inicial() e imprimir()
    Criamos a função para exibir as opções ao usuário e assim ele escolher se deseja digitar os números ou se deseja que eles sejam gerados aleatoriamente. A função imprimir exibe qual número é maior, menor e a média. E também os números em ordem crescente e decrescente.

--- questoes/questao2.py
+++ questoes/questao2.py
@@ -10,6 +10,28 @@ print("******** RAUL BARCELOS *********")
 print()
 
 
+def pagina_inicial(tamanho, lista):
+    lista.clear()
+
+    opcao_str = input("1 → Digitar números \n2 → Gerar aleatóriamente \n0 → Sair da questão\nSelecione a opção:")
+
+    if not opcao_str.isdigit():
+        print("\nOpção inexistente. Tente novamente!\n")
+        pagina_inicial(tamanho, lista)
+
+    opcao = int(opcao_str)
+    print()
+    if opcao == 1:
+        ler_dez_numero(tamanho, lista)
+    elif opcao == 2:
+        gerar_dez_numeros(tamanho, lista)
+    elif opcao == 0:
+        print("Saindo da questão!")
+        return
+    else:
+        print("Opção incorreta! Tente novamente.", end="\n\n")
+        pagina_inicial(tamanho, lista)
+
 
 def ler_dez_numero(tamanho, lista):
     contador = 0
@@ -36,3 +58,23 @@ def gerar_dez_numeros(tamanho, lista):
         numeros.append(int(random.randint(1, 100)))
 
     imprimir(tamanho, lista, "gerados")
+
+
+def imprimir(tamanho, lista, forma):
+    print("|-------------------------------------------|")
+    print("Os números {} foram:".format(forma))
+    print("→", lista, end="\n\n")
+    print("••• O maior valor é", max(lista))
+    print("••• O menor valor é", min(lista))
+    print("••• A média é", sum(lista) / tamanho, end="\n\n")
+    lista.sort()
+    print("Ordem crescente:")
+    print("→", lista)
+    print("Ordem decrescente:")
+    lista.reverse()
+    print("→", lista)
+    print("|-------------------------------------------|", end="\n\n")
+    pagina_inicial(tamanho, lista)
+
+
+pagina_inicial(define_tamanho, numeros)
